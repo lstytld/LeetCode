483d39c8cdf76defa0fb94541bfaf1bf43065e1b
Jerry Chen <zhwchen@ucdavis.edu>
2020-02-13 18:28:47 -0800

    2020/2/13

---
+++ 101 - 200/198. House Robber.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+
+        prev2 = 0
+        cur = nums[0]
+        for i in range(1, len(nums)):
+            prev1, prev2, cur = prev2, cur, max(cur, prev2 + nums[i])
+        return cur
+    
