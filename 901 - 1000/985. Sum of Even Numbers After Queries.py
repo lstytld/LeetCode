caa07862fd02271a14d3184b7037071972acc0ce
Jerry Chen <zhwchen@ucdavis.edu>
2020-02-14 16:24:30 -0800

    2020/2/13

---
+++ 901 - 1000/985. Sum of Even Numbers After Queries.py
@@ -0,0 +1,20 @@
+class Solution:
+    def sumEvenAfterQueries(self, A: List[int], queries: List[List[int]]) -> List[int]:
+        ans = []
+        start = 0
+        for i in A:
+            if i % 2 == 0:
+                start += i
+        for value, index in queries:
+            if A[index] % 2 == 0:
+                if value % 2 == 0:
+                    start += value
+                else:
+                    start -= A[index]
+            else:
+                if value % 2 == 1:
+                    start += A[index] + value
+            A[index] += value
+            ans.append(start)
+
+        return ans
