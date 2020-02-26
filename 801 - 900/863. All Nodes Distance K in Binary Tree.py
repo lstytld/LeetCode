6c124727a4d3da33048610ee18a043afd0404a86
Jerry Chen <zhwchen@ucdavis.edu>
2020-02-25 19:32:24 -0800

    2020/2/25

--- 801 - 900/863. All Nodes Distance K in Binary Tree.py
+++ 801 - 900/863. All Nodes Distance K in Binary Tree.py
@@ -41,3 +41,46 @@ class Solution(object):
 
         return ans
 
+class Solution(object):
+    def distanceK(self, root, target, K):
+        """
+        :type root: TreeNode
+        :type target: TreeNode
+        :type K: int
+        :rtype: List[int]
+        """
+        ans = []
+
+        def dfs(node):
+            if not node:
+                return -1
+            if node == target:
+                add_ans(node, K)
+                return 0
+            left, right = dfs(node.left), dfs(node.right)
+            if left != -1:
+                if left == K - 1:
+                    add_ans(node, 0)
+                if left < K - 1:
+                    add_ans(node.right, K - left - 2)
+                return left + 1
+            if right != -1:
+                if right == K - 1:
+                    add_ans(node, 0)
+                if right < K - 1:
+                    add_ans(node.left, K - right - 2)
+                return right + 1
+            return -1
+
+        def add_ans(node, k):
+            if not node or k < 0:
+                return
+            if k == 0:
+                ans.append(node.val)
+            else:
+                add_ans(node.left, k - 1)
+                add_ans(node.right, k - 1)
+            return
+
+        dfs(root)
+        return ans
